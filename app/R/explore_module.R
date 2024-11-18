e53dc15bb439860d169bd2a6333408fd6ea1820a
Paul McBride <paul@paul.mcbride.nz>
2024-11-18 04:53:00 +0000

    - improved SA2 loading for smoother panning

--- app/R/explore_module.R
+++ app/R/explore_module.R
@@ -5,21 +5,21 @@ explore_ui <- function(id) {
   layout_column_wrap(
     width = NULL,
     style = css(grid_template_columns = "70% 30%"),
-  card(
-    card_header(
-      selectizeInput(ns("var"), NULL, choices=var_list)
-    ),
-    card_body(
-      leafletOutput(ns("explore_map"))
+    card(
+      card_header(
+        selectizeInput(ns("var"), NULL, choices=var_list)
+      ),
+      card_body(
+        leafletOutput(ns("explore_map"))
       )
-  ),
-  card(
-    card_header(
-      "Head placeholder"
     ),
-    card_body(
-      "Body placeholder")
-      )
+    card(
+      card_header(
+        "Head placeholder"
+      ),
+      card_body(
+        "Body placeholder")
+    )
   )
 }
 
@@ -28,7 +28,7 @@ explore_server <- function(id) {
 
   moduleServer(id, function(input, output, session) {
 
-  map <- map_start()
+    map <- map_start()
 
     var <- reactive({
       if_else(input$var == var_list[1], "dummy", input$var)
@@ -42,7 +42,12 @@ explore_server <- function(id) {
       input$explore_map_zoom
     })
 
+    pal_sa2 <- eventReactive(var(), {
+      palette_maker(domain = sa2[[var()]])
+    })
+
     current_ta <- reactiveVal("none")
+    current_sa2 <- reactiveVal(NULL)
     proxy_ta <- reactiveVal("none")
     proxy_var <- reactiveVal("dummy")
 
@@ -70,24 +75,23 @@ explore_server <- function(id) {
 
     observeEvent(list(var(),add_layer()), {
 
-      pal_sa2 <- palette_maker(domain = sa2[[var()]])
-      # pal_ta <- palette_maker(domain = ta[[var()]])
-
       if(is_empty(add_layer())) {
         sa2_in <- sa2[1,]
         sa2_opacity <- 0
       } else {
-        sa2_in <- add_layer()
+        sa2_in <- add_layer() %>%
+          filter(!SA22023_V1 %in% current_sa2())
+        current_sa2(c(current_sa2(), sa2_in$SA22023_V1))
         sa2_opacity <- main_opacity
       }
       ta_trim_ <- ta[!ta$TALB2023_1 %in% add_layer()$ta,]
-      if(!identical(var(), proxy_var())  | !identical(current_ta(),proxy_ta())) {
+      if(!identical(current_ta(),proxy_ta()) &&
+         map_zoom_() >= 10) {
         leafletProxy("explore_map", session) %>%
-          clearGroup(group="sa2") %>%
           clearGroup(group="ta") %>%
           clearControls() %>%
           addPolygons(data=sa2_in,
-                      fillColor = ~pal_sa2(sa2_in[[var()]]),
+                      fillColor = ~pal_sa2()(sa2_in[[var()]]),
                       weight = 1,
                       smoothFactor = 0.5,
                       opacity = 0.2,
@@ -97,7 +101,7 @@ explore_server <- function(id) {
                       highlightOptions = base_leaflet_highlight,
                       group="sa2") %>%
           addPolygons(data=ta_trim_,
-                      fillColor = ~pal_sa2(ta_trim_[[var()]]),
+                      fillColor = ~pal_sa2()(ta_trim_[[var()]]),
                       weight = 1,
                       smoothFactor = 0.5,
                       opacity = 0.2,
@@ -115,7 +119,101 @@ explore_server <- function(id) {
           addLegend(layerId = "legend_polygon",
                     data=sa2,
                     position = "topright",
-                    pal = pal_sa2,
+                    pal = pal_sa2(),
+                    values = ~na.omit(sa2[[var()]]),
+                    title = " ",
+                    na.label = NULL)
+        proxy_ta(current_ta())
+        proxy_var(var())
+      }
+      if(!identical(current_ta(),proxy_ta()) &&
+         map_zoom_() < 10) {
+        leafletProxy("explore_map", session) %>%
+          clearGroup(group="sa2") %>%
+          clearGroup(group="ta") %>%
+          clearControls() %>%
+          addPolygons(data=sa2_in,
+                      fillColor = ~pal_sa2()(sa2_in[[var()]]),
+                      weight = 1,
+                      smoothFactor = 0.5,
+                      opacity = 0.2,
+                      fillOpacity = sa2_opacity,
+                      color = ~"black",
+                      layerId = ~SA22023_V1,
+                      highlightOptions = base_leaflet_highlight,
+                      group="sa2") %>%
+          addPolygons(data=ta,
+                      fillColor = ~pal_sa2()(ta[[var()]]),
+                      weight = 1,
+                      smoothFactor = 0.5,
+                      opacity = 0.2,
+                      fillOpacity = main_opacity,
+                      color = ~"black",
+                      layerId = ~TALB2023_1,
+                      highlightOptions = base_leaflet_highlight,
+                      group="ta") %>%
+          addControl("Showing territorial authorities",
+                     position = "topleft") %>%
+          addControl(var(),
+                     position = "topright") %>%
+          addLegend(layerId = "legend_polygon",
+                    data=sa2,
+                    position = "topright",
+                    pal = pal_sa2(),
+                    values = ~na.omit(sa2[[var()]]),
+                    title = " ",
+                    na.label = NULL)
+        proxy_ta(current_ta())
+        proxy_var(var())
+        current_sa2(NULL)
+      }
+    })
+
+    observeEvent(list(var()), {
+
+      if(is_empty(add_layer())) {
+        sa2_in <- sa2[1,]
+        sa2_opacity <- 0
+      } else {
+        sa2_in <- add_layer()
+        sa2_opacity <- main_opacity
+      }
+      ta_trim_ <- ta[!ta$TALB2023_1 %in% add_layer()$ta,]
+      if(!identical(var(), proxy_var())) {
+        leafletProxy("explore_map", session) %>%
+          clearGroup(group="sa2") %>%
+          clearGroup(group="ta") %>%
+          clearControls() %>%
+          addPolygons(data=sa2_in,
+                      fillColor = ~pal_sa2()(sa2_in[[var()]]),
+                      weight = 1,
+                      smoothFactor = 0.5,
+                      opacity = 0.2,
+                      fillOpacity = sa2_opacity,
+                      color = ~"black",
+                      layerId = ~SA22023_V1,
+                      highlightOptions = base_leaflet_highlight,
+                      group="sa2") %>%
+          addPolygons(data=ta_trim_,
+                      fillColor = ~pal_sa2()(ta_trim_[[var()]]),
+                      weight = 1,
+                      smoothFactor = 0.5,
+                      opacity = 0.2,
+                      fillOpacity = main_opacity,
+                      color = ~"black",
+                      layerId = ~TALB2023_1,
+                      highlightOptions = base_leaflet_highlight,
+                      group="ta") %>%
+          addControl(if_else(map_zoom_() >=10,
+                             "Showing communities (statistical area 2)",
+                             "Showing territorial authorities"),
+                     position = "topleft") %>%
+          addControl(var(),
+                     position = "topright") %>%
+          addLegend(layerId = "legend_polygon",
+                    data=sa2,
+                    position = "topright",
+                    pal = pal_sa2(),
                     values = ~na.omit(sa2[[var()]]),
                     title = " ",
                     na.label = NULL)
